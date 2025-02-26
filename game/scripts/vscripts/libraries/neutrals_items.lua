d3b93333884baad88d4eb99b03ff06103fba0100
Plarget <67307148+Plarget@users.noreply.github.com>
2025-02-26 16:24:32 +0400

    fixes

--- game/scripts/vscripts/libraries/neutrals_items.lua
+++ game/scripts/vscripts/libraries/neutrals_items.lua
@@ -1,13 +1,18 @@
 function CAddonWarsong:GiveNeutralsItemsForPlayers(nTime)
     local nNextTier = (self.nLastTierDropped or 0) + 1
     local nTierTiming = NEUTRAL_ITEM_TIMINGS['TIER_' .. nNextTier]
+    local teamMadstone = {}
+
     if nTierTiming and nTime >= nTierTiming then
         self.nLastTierDropped = nNextTier
         for _, entity in pairs( HeroList:GetAllHeroes() ) do
             if not entity:IsNull() and entity:IsRealHero() and not entity:HasModifier("modifier_monkey_king_fur_army_soldier") and not entity:HasModifier("modifier_monkey_king_fur_army_soldier_hidden") and not entity:IsClone() and not entity:IsTempestDouble() then
-                local neutral_item_name = "item_tier"..nNextTier.."_token"
-                local item = entity:AddItemByName(neutral_item_name)
-            end
+                -- local neutral_item_name = "item_tier"..nNextTier.."_token"
+                if not teamMadstone[tostring(entity:GetTeamNumber())] then 
+                    local item = entity:AddItemByName("item_madstone_bundle")
+                    teamMadstone[tostring(entity:GetTeamNumber())] = true
+                end
+             end
         end
     end
 end
