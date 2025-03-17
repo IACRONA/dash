a2ffbbbb70495ec613055164147f6fc03b1955c8
SniperX <40890777+MaelstromDota@users.noreply.github.com>
2025-03-17 16:54:35 +0500

    rating system

--- game/scripts/vscripts/server/server_manager.lua
+++ game/scripts/vscripts/server/server_manager.lua
@@ -1,12 +1,12 @@
 if ServerManager == nil then
 	ServerManager = class({})
 end
- 
+
 function ServerManager:Init()
-	
+
 	DoWithAllPlayers(function(_,_,playerId)
 		local steamId = PlayerResource:GetSteamAccountID(playerId)
- 
+
 		HTTP("POST", "/player/".. steamId, nil, {success = function(data)
 			PlayerInfo:InitPlayer(playerId, data)
 		end})
@@ -14,23 +14,23 @@ function ServerManager:Init()
 
   	Timers:CreateTimer(2, function()
  		HTTP("POST", "/store/store_info", nil, {success = function(data)
-			CustomNetTables:SetTableValue("server_info", "store", data) 
+			CustomNetTables:SetTableValue("server_info", "store", data)
 		end})
  	end)
 
 	CustomGameEventManager:RegisterListener('buy_item', Dynamic_Wrap(self, 'BuyItem'))
 	CustomGameEventManager:RegisterListener('equip_shop_item', Dynamic_Wrap(self, 'EquipShopItem'))
 end
- 
+
 
 function ServerManager:BuyItem(data)
 	local playerId = data.playerId
 	local steamId = PlayerResource:GetSteamAccountID(playerId)
 	local player = PlayerResource:GetPlayer(playerId)
- 
-	HTTP("POST", "/store/buy_item", {id = steamId, item_name = data.item, type = data.type}, {success = function(data) 
-		PlayerInfo:UpdatePlayerTable(playerId, data.user)      
-		end, 
+
+	HTTP("POST", "/store/buy_item", {id = steamId, item_name = data.item, type = data.type}, {success = function(data)
+		PlayerInfo:UpdatePlayerTable(playerId, data.user)
+		end,
 		error = function(data)
 			CustomGameEventManager:Send_ServerToPlayer(player, "store_error", {error = data.error})
 		end,
@@ -43,23 +43,27 @@ end
 function ServerManager:SendServerPlayerRoll(playerId)
 	local steamId = PlayerResource:GetSteamAccountID(playerId)
 	local rollUsed = PlayerInfo:GetRollUsedPlayer(playerId)
-	local playerInfo = CustomNetTables:GetTableValue("player_info", tostring(playerId)) 
+	local playerInfo = CustomNetTables:GetTableValue("player_info", tostring(playerId))
 
 	if not playerInfo then return end
-	
+
 	local newRoll = (playerInfo.roll or 0) - rollUsed
 	HTTP("POST", "/player", {id = steamId, roll = newRoll}, {
-		success = function(data) 
+		success = function(data)
 			PlayerInfo:UpdateRollTable(playerId, 0, -rollUsed)
-			PlayerInfo:UpdatePlayerTable(playerId, data)      
-		end, 
+			PlayerInfo:UpdatePlayerTable(playerId, data)
+		end,
 	})
 end
 
 function ServerManager:OnEndGame(callback)
+	if self._game_end == true then return end
+	self._game_end = true
+
 	local playersReady = {}
+	local matchSent = false
 	local checkPlayers = function()
-		local canEnd = true
+		local canEnd = matchSent
 		LogPanorama(playersReady)
 
 		DoWithAllPlayers(function(_,_,id)
@@ -70,12 +74,32 @@ function ServerManager:OnEndGame(callback)
 	end
 
 	DoWithAllPlayers(function(player,_,playerId)
+		local team = PlayerResource:GetTeam(playerId)
+		if GameRules.AddonTemplate.nCapturedFlagsCount[team] == nil then
+			GameRules.AddonTemplate.nCapturedFlagsCount[team] = 0
+		end
+	end)
+
+	local sortedTeams = {}
+	for team, kills in pairs( GameRules.AddonTemplate.nCapturedFlagsCount ) do
+		table.insert( sortedTeams, { teamID = team, teamScore = kills } )
+	end
+	table.sort( sortedTeams, function(a,b) return ( a.teamScore > b.teamScore ) end )
+
+	local teamPlaces = {}
+	for i, data in pairs(sortedTeams) do
+		teamPlaces[data.teamID] = i
+	end
+
+	local places = {}
+
+	DoWithAllPlayers(function(player,_,playerId)
 		local steamId = PlayerResource:GetSteamAccountID(playerId)
 		LogPanorama(playersReady)
 
 		LogPanorama(steamId)
-		
-		if not player or steamId == -1 or steamId == 0 then 
+
+		if not player or steamId == -1 or steamId == 0 then
 			playersReady[playerId] = true
 			checkPlayers()
 			return
@@ -83,9 +107,9 @@ function ServerManager:OnEndGame(callback)
 		LogPanorama(playersReady)
 
 		local rollUsed = PlayerInfo:GetRollUsedPlayer(playerId)
-		local playerInfo = CustomNetTables:GetTableValue("player_info", tostring(playerId)) 
-	
-		if not playerInfo then 
+		local playerInfo = CustomNetTables:GetTableValue("player_info", tostring(playerId))
+
+		if not playerInfo then
 			playersReady[playerId] = true
 			return checkPlayers()
 		end
@@ -99,7 +123,29 @@ function ServerManager:OnEndGame(callback)
 				checkPlayers()
  			end,
 		})
+
+		if not PlayerResource:IsFakeClient(playerId) then
+			local place = teamPlaces[PlayerResource:GetTeam(playerId)]
+			if places[tostring(place)] == nil then
+				places[tostring(place)] = {tostring(steamId)}
+			else
+				table.insert(places[tostring(place)], tostring(steamId))
+			end
+		end
 	end)
+
+	-- if not GameRules:IsCheatMode() or not IsDedicatedServer() then
+	if true then
+		HTTP("POST", "/match/result", {places = places, map_name = GetMapName()}, {
+			finnaly = function()
+				matchSent = true
+				checkPlayers()
+			end,
+		})
+	else
+		matchSent = true
+		checkPlayers()
+	end
 
 	Timers:CreateTimer(10, function()
 		callback()
@@ -110,11 +156,11 @@ function ServerManager:EquipShopItem(data)
 	local playerId = data.playerId
 	local steamId = PlayerResource:GetSteamAccountID(playerId)
 	local player = PlayerResource:GetPlayer(playerId)
- 
+
 	HTTP("POST", "/equip_item", {id = steamId, item_name = data.item, type = data.type}, {
-		success = function(data) 
-			PlayerInfo:UpdatePlayerTable(playerId, data)      
-		end, 
+		success = function(data)
+			PlayerInfo:UpdatePlayerTable(playerId, data)
+		end,
 		error = function(data)
 			CustomGameEventManager:Send_ServerToPlayer(player, "store_error", {error = data.error})
 		end,
@@ -123,7 +169,7 @@ function ServerManager:EquipShopItem(data)
 		end,
 	})
 end
- 
+
 
 
 
